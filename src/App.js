1c7310549be5c1af68a31dfea6e295b7cb752745
ALsaead323 <alsaead2110679@miuegypt.edu.eg>
2024-06-25 15:18:05 +0300

    just rebuild another project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Todo from'./components/Todo'
+
 
 function App() {
   return (
     <div >
-      <h1>My Todos</h1>
-     <Todo text='Hello '/>
-     <Todo text='ZERO '/>
-     <Todo text='Hacker '/>
+  
 
     </div>
   );
