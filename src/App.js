88b5f18c16780e27e55510fd1a2f74207ea8132d
ALsaead323 <alsaead2110679@miuegypt.edu.eg>
2024-06-25 16:12:35 +0300

    just rebuild another project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {Route} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetup from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
 
 
 function App() {
   return (
     <div >
   
+  <Route path='/'>
+  <AllMeetupsPage/>
+  </Route>
 
+  <Route path='/new-meetup'>
+  <NewMeetup/>
+  </Route>
+  
+  <Route path='/fav-meetup'>
+  <Favorites/>
+  </Route>
     </div>
   );
 }
