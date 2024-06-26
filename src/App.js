ce686d25e39107a74cf214b6709693519973d15a
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-06-26 20:52:04 +0300

    handle layout  components

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-    <MainNavigation/>
+    <Layout>
+   
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetup />} />
         <Route path='/fav-meetup' element={<Favorites />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/meetup/MeetupItem.js
+++ src/components/meetup/MeetupItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css'
 
 function MeetupItem(props) {
     return (
         <li className={classes.item}>
+            <Card>
             <div>
                 <h3>{props.title}</h3>
                 <img className={classes.image} src={props.image} alt={props.title} />
                 <p className={classes.content}>{props.description}</p>
             </div>
             <div className={classes.actions}> <button>To Favorites</button></div>
-           
+            </Card>
         </li>
       
     );
