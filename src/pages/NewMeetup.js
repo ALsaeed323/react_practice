41516d06591b101957216127adbc275383885a53
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-06-26 22:14:27 +0300

    handle form  components

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,12 @@
+import NewMeetupForm from "../components/meetup/NewMeetupForm";
+
 function NewMeetup (){
 
     return(
-        <div>NewMeetup</div>
+       <section>
+        <h1>ADD New Meetup</h1>
+        <NewMeetupForm/>
+       </section>
     );
 }
 export default NewMeetup;
