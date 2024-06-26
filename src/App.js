9b3861c4a8dae7b301ff2ba64da2a181f477ff8b
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-06-26 13:50:55 +0300

    handle links component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetup />} />
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+
 function MainNavigation ()
 {
 
     return (
         <header>
-            <div></div>
+            <div>React meetups </div>
+            <nav>
+                <ul>
+                    <li>
+                       <Link to="/">ALL meetups</Link>
+                      <br/>
+                       <Link to="/fav-meetup">ALL fav-meetup</Link>
+                       <br/>
+                       <Link to="/new-meetup">ALL new-meetup</Link>
+                       <br/>
+                       <Link to= "https://www.linkedin.com/in/alsaead-aziz-b19965249" target="_blank" rel="noopener noreferrer">MY LinkedIN </Link> 
+                    </li>
+                </ul>
+            </nav>
+
         </header>
     );
 }
