5a8989824b1fa9bf64193f0f8a35971f30e0291c
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-06-27 13:27:24 +0300

    handle favorites page and make ir dynamicly

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,22 @@
-function Favorites (){
+import { useContext } from "react";
+import { FavoritesContext } from "../store/favorites-context";
+import MeetupList from "../components/meetup/MeetupList";
 
+function Favorites (){
+    const favoritesCtx =useContext(FavoritesContext);
+
+    let content;
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>Got no favorites yet. Start adding!</p>;
+    } else {
+        content = <MeetupList  items={favoritesCtx.favorites} />;
+    }
+    
     return(
-        <div>Favorites</div>
+        <section>
+        <h1>My Favorites</h1>
+        {content}
+        </section>
     );
 }
 export default Favorites;
