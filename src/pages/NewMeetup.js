61ae69d680a738525aecd7622fe1309f30acca83
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-06-27 11:24:33 +0300

    handle connection with firebase and retrive the data from it to be dynamicly

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,34 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetup/NewMeetupForm";
 
-function NewMeetup (){
+function NewMeetup() {
+    const navigate = useNavigate();
+    const [successMessage, setSuccessMessage] = useState('');
 
-    return(
-       <section>
-        <h1>ADD New Meetup</h1>
-        <NewMeetupForm/>
-       </section>
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-5053d-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            setSuccessMessage('Data Saved Successfully');
+            console.log(response);
+            navigate('/', { replace: true });
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+            {successMessage && <p>{successMessage}</p>}
+        </section>
     );
 }
+
 export default NewMeetup;
